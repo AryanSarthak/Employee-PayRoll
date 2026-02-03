0f7886b0220e30b411b8116739f91232f6379e7d
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-03 19:11:43 +0530

    [Aryan] :Initial Project Setup

---
+++ src/test/java/org/gevernova/employeepayroll/EmployeePayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gevernova.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayRollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
